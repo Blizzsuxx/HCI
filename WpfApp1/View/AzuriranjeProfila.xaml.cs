1c7cef8cd6248c7debdfdcbe993afe4f2ef9d930
Blizzsuxx <draganarsic1999@hotmail.com>
2021-06-08 10:20:39 +0200

    odradjen backend za azuriranje

--- WpfApp1/View/AzuriranjeProfila.xaml.cs
+++ WpfApp1/View/AzuriranjeProfila.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using organizerEvents.Controler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.Controler;
 
 namespace WpfApp1.View
 {
@@ -22,6 +25,27 @@ namespace WpfApp1.View
         public AzuriranjeProfila()
         {
             InitializeComponent();
+            ime.Text = DataBase.trenutniKorisnik.Ime;
+            prezime.Text = DataBase.trenutniKorisnik.Prezime;
+            email.Text = DataBase.trenutniKorisnik.Email;
+            brTel.Text = DataBase.trenutniKorisnik.BrojTelefona;
+            korIme.Text = DataBase.trenutniKorisnik.KorisnickoIme;
+        }
+
+        public void proveriSifru(object sender, RoutedEventArgs e)
+        {
+            if (lozinka.Text.Length < 8)
+            {
+                lozinka.BorderBrush = Brushes.DarkRed;
+                HintAssist.SetHelperText(lozinka, "Sifra mora imati 8 ili vise karaktera");
+                HintAssist.SetForeground(lozinka, Brushes.DarkRed);
+                lozinka.Foreground = Brushes.DarkRed;
+            }
+            else
+            {
+                lozinka.BorderBrush = Brushes.Black;
+                lozinka.Foreground = Brushes.Black;
+            }
         }
 
         private void SacuvajPromene_Click(object sender, RoutedEventArgs e)
@@ -32,6 +56,43 @@ namespace WpfApp1.View
             String KorisnickoIme = korIme.Text;
             String Email = email.Text;
             String Lozinka = lozinka.Text;
+
+
+
+
+            try
+            {
+                UsernameValidator usernameValidator = new UsernameValidator();
+                Validator.validate(KorisnickoIme, usernameValidator);
+
+                NoEmptyFieldValidator noEmptyFieldValidator = new NoEmptyFieldValidator();
+                Validator.validate(Ime, noEmptyFieldValidator);
+                Validator.validate(Prezime, noEmptyFieldValidator);
+
+                EmailValidator emailValidator = new EmailValidator();
+                Validator.validate(Email, emailValidator);
+
+                NumbersValidator numbersValidator = new NumbersValidator();
+                Validator.validate(BrTel, numbersValidator);
+                if (Lozinka.Length < 8 && Lozinka.Length != 0)
+                    throw new Exception("Molimo vas da ispravno popunite sva polja " + "sifra" + " je nevalidna");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Molimo vas da ispravno popunite sva polja " + ex.Message + " je nevalidan");
+                return;
+            }
+            MessageBox.Show("Uspesno ste se promenili podatke!");
+            DataBase.trenutniKorisnik.Ime = Ime;
+            DataBase.trenutniKorisnik.Prezime = Prezime;
+            DataBase.trenutniKorisnik.Email = Email;
+            DataBase.trenutniKorisnik.KorisnickoIme = KorisnickoIme;
+            DataBase.trenutniKorisnik.BrojTelefona = BrTel;
+            if (Lozinka.Length != 0)
+            {
+                DataBase.trenutniKorisnik.Ime = Ime;
+            }
+            this.Close();
 
         }
 
